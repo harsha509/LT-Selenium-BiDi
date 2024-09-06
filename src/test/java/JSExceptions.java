f83fc6ac41d5863a13ac987ec247f0557757f327
Sri Harsha <sri_harsha509@hotmail.com>
2024-09-05 20:00:09 -0400

    Add: adding initial bidi repo examples

---
+++ src/test/java/JSExceptions.java
@@ -0,0 +1,71 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.bidi.module.LogInspector;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.Augmenter;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+
+public class JSExceptions {
+
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+    String status = "passed";
+    String LT_USERNAME = System.getenv("LT_USERNAME");
+    String LT_KEY = System.getenv("LT_ACCESS_KEY");
+
+    @BeforeSuite
+    public void setup() throws MalformedURLException {
+        FirefoxOptions options = new FirefoxOptions();
+        options.setPlatformName("Windows 10");
+        options.setBrowserVersion("127");
+        HashMap<String, Object> ltOptions = new HashMap<String, Object>();
+        ltOptions.put("username", LT_USERNAME);
+        ltOptions.put("accessKey",LT_KEY );
+        ltOptions.put("build", "BiDi - Log Events");
+        ltOptions.put("name", "JSExceptions");
+        ltOptions.put("project", "BiDi");
+        ltOptions.put("w3c", true);
+        ltOptions.put("plugin", "java-testNG");
+        options.setCapability("LT:Options", ltOptions);
+        options.setCapability("webSocketUrl", true);
+        driver = new RemoteWebDriver(new URL("https://hub.lambdatest.com/wd/hub"), options);
+        driver = new Augmenter().augment(driver);
+    }
+
+    @Test
+    public void jsExceptions() {
+        try (LogInspector logInspector = new LogInspector(driver)) {
+            logInspector.onJavaScriptException(logEntry -> {
+                System.out.println("text: " + logEntry.getText());
+                System.out.println("level: " + logEntry.getLevel());
+            });
+
+            String page = "https://www.selenium.dev/selenium/web/bidi/logEntryAdded.html";
+            driver.get(page);
+            driver.findElement(By.id("jsException")).click();
+        }
+    }
+
+    @AfterTest
+    public void teardown() {
+        if (driver != null) {
+            ((JavascriptExecutor) driver).executeScript("lambda-status=" + status);
+            driver.quit();
+        }
+    }
+
+    @AfterSuite
+    public void close() {
+        driver.quit();
+    }
+}
